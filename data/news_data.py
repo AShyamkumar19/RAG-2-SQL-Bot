4283d0baa00bdd423540ee1eb3c5e9780710909b
aarav <saarav.as19@gmail.com>
2024-08-30 16:33:31 -0400

    Data files

--- data/news_data.py
+++
@@ -1,9 +0,0 @@
-import tweepy
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-tweets=api.user_timeline(screen_name="",count=100,tweet_mode="extended")
-print(f"No of tweets exracted:{len(tweets)}")
-
-# Codes still needs to be finsihed by getting the keys
